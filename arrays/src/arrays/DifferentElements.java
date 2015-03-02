cf4fbbca3b93886a276743346957e0f17df6595c
Abhishek Bhadange <abhishekbhadange@gmail.com>
2015-03-01 19:39:55 -0800

    Two different elements among two sorted arrays

---
+++ arrays/src/arrays/DifferentElements.java
@@ -0,0 +1,48 @@
+package arrays;
+
+public class DifferentElements {
+	public static void main(String[] args) {
+		int[] arr1 = {2, 5, 9, 10, 15, 18};
+		int[] arr2 = {2, 5, 9, 10, 11, 15, 18, 19};
+		printDifferentElements(arr1, arr2);
+	}
+	public static void printDifferentElements(int[] arr1, int[] arr2) {
+		int[] biggerArray;
+		int[] smallerArray;
+		int n;
+		if(arr1.length > arr2.length) {
+			biggerArray = arr1;
+			smallerArray = arr2;
+			n = arr2.length;
+		}
+		else {
+			biggerArray = arr2;
+			smallerArray = arr1;
+			n = arr1.length;
+		}
+		int count = 0;
+		int i = 0;
+		int j = 0;
+		while(i < n) {
+			if(arr1[i] == arr2[j]) {
+				i++;
+				j++;
+			}
+			else {
+				System.out.print(arr2[j] + " ");
+				count++;
+				j++;
+			}
+		}
+		if(count == 1) {
+			if(biggerArray[biggerArray.length-2] == smallerArray[smallerArray.length-1])
+				System.out.print(biggerArray[biggerArray.length-1] + " ");
+			else
+				System.out.print(biggerArray[biggerArray.length-2] + " ");
+		}
+		else if(count == 0) {
+			System.out.print(biggerArray[biggerArray.length-2] + " ");
+			System.out.print(biggerArray[biggerArray.length-1] + " ");
+		}
+	}
+}
